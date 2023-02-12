e728f4176cc6e86705dded3df2a88c42d5ea38e6
Connor <connoraaugustine@gmail.com>
2023-02-12 11:56:15 -0600

    02/12/2023 Update to C++

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main() {
     cout<<"Hello World!\n";
-    cout<<"Hoping to add a second line of text below";
-
+    cout<<"Hoping to add a second line of text below\n";
+    for(int i = 1; i < 11; i++) {
+        cout<<"" << i << ".\n";
+    }
     return 0;
 }
